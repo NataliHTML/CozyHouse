b60b873838f630c49fde95683865ea9895581cda
NataliHTML <nataly07081992@gmail.com>
2021-11-18 23:33:38 +0300

    added dogs

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,39 @@
 $(function () {
-
-
+	$('.menu__btn').on('click', function () {
+		$('.menu__list').toggleClass('menu__list--active');
+	});
+	$('.menu__list').on('click', function () {
+		$('.menu__list').removeClass('menu__list--active');
+	});
+	$('.friends__slider').slick({
+		nextArrow: '<button class="slick-arrow slick-next"><img src="images/icons/right-arrow.png" alt="next arrow"></button>',
+		prevArrow: '<button class="slick-arrow slick-prev"><img src="images/icons/left-arrow.png" alt="prev arrow"></button>',
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		responsive: [
+			{
+				breakpoint: 1300,
+				settings: {
+					arrows: false,
+					dots: true,
+				}
+			},
+			{
+				breakpoint: 920,
+				settings: {
+					arrows: false,
+					dots: true,
+					slidesToShow: 2,
+				}
+			},
+			{
+				breakpoint: 620,
+				settings: {
+					arrows: false,
+					dots: true,
+					slidesToShow: 1,
+				}
+			},
+		]
+	});
 });
