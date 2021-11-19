ebc77c9fcb3703b749012d4d9f2d4bdaa8f08731
NataliHTML <nataly07081992@gmail.com>
2021-11-19 19:32:14 +0300

    fixed header scroll tel,mail

--- app/js/main.js
+++ app/js/main.js
@@ -36,4 +36,14 @@ $(function () {
 			},
 		]
 	});
+	$('[data-scroll]').on('click', function (event) {
+		event.preventDefault();
+
+		blockId = $(this).data('scroll');
+		blockOffset = $(blockId).offset().top;
+
+		$("html,body").animate({
+			scrollTop: blockOffset + 50
+		}, 700);
+	});
 });
